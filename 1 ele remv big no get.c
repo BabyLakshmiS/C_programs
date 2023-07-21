5c0e85bdbb7b3e8adceb331d1b5d51555305cf7b
BabyLakshmiS <132995814+BabyLakshmiS@users.noreply.github.com>
2023-07-21 16:11:53 +0530

    Add files via upload

---
+++ 1 ele remv big no get.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("enter n value ");
+    scanf("%d",&n);;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+     scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+     printf("%d",arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+
+
+    }
+}
